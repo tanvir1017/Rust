7c60d36ecb79503fbabca6b9f98191403465224f
tanvir1017 <developer.tanvirhossain@gmail.com>
2024-10-20 22:58:06 +0600

    feat: 🎸 Function chapter is done

--- chapter_3.2/function/src/main.rs
+++ chapter_3.2/function/src/main.rs
@@ -4,12 +4,23 @@ fn is_even(n: i32) -> bool {
     };
 
     println!("The {n} is not a even number");
-
-    false
+    false // we can write return keyword but it's not required just do not put a semicolon at the end of expression. Yes its called expression
 }
 
 fn main() {
     let x = is_even(6);
 
-    println!("{x}")
+    println!("{x}");
+    just_print();
+    println!("The added two number will return `{}`", add_two_num(5, 5));
+}
+
+
+fn just_print(){
+    println!("hello this is just return void")
+}
+
+fn add_two_num(x: i32, y:i32) -> i32{
+    let add: i32 = x + y;
+    add
 }
